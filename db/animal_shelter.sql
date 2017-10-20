abd2002b30ed5b0635dd75e9e23e51078fdb703d
Rob <robdwilliams85@gmail.com>
2017-10-20 18:27:06 +0100

    Initial sql file

---
+++ db/animal_shelter.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS owners;
+
+CREATE TABLE owners (
+  id SERIAL4 PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  address VARCHAR(255),
+  email VARCHAR(255),
+  phone_number INT4
+);
+
+CREATE TABLE animals (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  type VARCHAR(255),
+  adoptable BOOLEAN,
+  admission_date ,
+  adopted_date ,
+  owner_id INT4 REFERENCES owners(id)
+);
