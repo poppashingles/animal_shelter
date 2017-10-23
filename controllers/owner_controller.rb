f50273f5dbec13d880965a3dd58428937e941489
Rob <robdwilliams85@gmail.com>
2017-10-23 09:44:37 +0100

    Owner create function working

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -8,6 +8,17 @@ get '/owners/index' do
   erb(:"owners/index")
 end
 
+get '/owners/new' do
+  erb(:"owners/new")
+end
+
+post '/owners' do
+  @owner = Owner.new(params)
+  @owner.save()
+  erb(:"owners/create")
+end
+
+
 get '/owners/:id' do
   @owner = Owner.find(params[:id])
   erb(:"owners/show")
