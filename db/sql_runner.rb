b5e2d97c6412b76e203b89d317fa419fba79af3c
Rob <robdwilliams85@gmail.com>
2017-10-30 13:18:13 +0000

    added pw and host to sql_runner

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -3,7 +3,7 @@ class SqlRunner
 
   def self.run( sql, values )
     begin
-      db = PG.connect({ dbname: 'd1ods88rld8ccr', host: 'ec2-184-73-247-240.compute-1.amazonaws.com' })
+      db = PG.connect({ dbname: 'd1ods88rld8ccr', password: 'ce69322e974540be9cf11ea5ae9117babf359a0cd58b95da094d352d365da0e6', port: 5432, host: 'ec2-184-73-247-240.compute-1.amazonaws.com' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
